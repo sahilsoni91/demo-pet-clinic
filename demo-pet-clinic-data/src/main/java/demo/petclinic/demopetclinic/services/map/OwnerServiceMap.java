90a998ad9da2ccc41768a4893ceaba53679452cc
Sahil Soni <E01602@hsc.com>
2019-12-23 17:02:22 +0530

    Map based implementation of services

--- demo-pet-clinic-data/src/main/java/demo/petclinic/demopetclinic/services/map/OwnerServiceMap.java
+++ demo-pet-clinic-data/src/main/java/demo/petclinic/demopetclinic/services/map/OwnerServiceMap.java
@@ -9,7 +9,7 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
 	@Override
 	public Owner save(Owner object) {
-		return super.save(object);
+		return super.save(object.getId(), object);
 	}
 
 	@Override
