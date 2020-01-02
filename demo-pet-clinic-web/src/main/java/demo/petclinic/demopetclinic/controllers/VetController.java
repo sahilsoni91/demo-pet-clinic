9febe401e06d99c996f2a61c5d0158be5ca43b78
Sahil Soni <E01602@hsc.com>
2020-01-02 17:50:48 +0530

    Added vets on Vets Index page

--- demo-pet-clinic-web/src/main/java/demo/petclinic/demopetclinic/controllers/VetController.java
+++ demo-pet-clinic-web/src/main/java/demo/petclinic/demopetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package demo.petclinic.demopetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import demo.petclinic.demopetclinic.services.VetService;
 
 @Controller
 public class VetController {
+	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
 
 	@RequestMapping(value = {"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vetsList", vetService.findAll());
 		return "vets/index";
 	}
 }
