e7d289188fc5b87b7166a7a8179931a4cc9aa0f3
Sahil Soni <E01602@hsc.com>
2020-01-02 17:08:35 +0530

    Added owners on Owner Index page

--- demo-pet-clinic-web/src/main/java/demo/petclinic/demopetclinic/controllers/OwnerController.java
+++ demo-pet-clinic-web/src/main/java/demo/petclinic/demopetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package demo.petclinic.demopetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import demo.petclinic.demopetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+	
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
 
 	@RequestMapping(value = {"", "/", "/index", "/index.html"})
-	public String listOwners() {
+	public String listOwners(Model model) {
+		model.addAttribute("ownersList", ownerService.findAll());
 		return "owners/index";
 	}
 }
